18d08d0068cda95f3a188ce3f39008ad90faecf1
gabrielhgcamargo <gacamargos4@gmail.com>
2022-01-27 16:37:31 -0300

    first commit

---
+++ src/main/java/io/github/gabrielhgcamargo/MarketStockApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.gabrielhgcamargo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketStockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketStockApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/gabrielhgcamargo/MarketStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gabrielhgcamargo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketStockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
